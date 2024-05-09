276183c07e4b49b51d9ec784e0a775003c734029
Zainab990 <zainabomar998@gmail.com>
2024-05-09 13:02:54 -0700

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,9 +13,11 @@ int check_cycle(listint_t *list)
 
     while (fast && fast->next)
     {
-	        slow = slow->next;
-	        fast = fast->next;
-		if (slow==fast)
-        		return (1);
+        slow = slow->next;
+        fast = fast->next;
+	if (slow == fast)
+        	return (1);
     }
+
     return (0);
+}
