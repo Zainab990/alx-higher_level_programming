f897817c99f4ef0ff6150982f308574055c8ba25
Zainab990 <zainabomar998@gmail.com>
2024-05-09 12:56:13 -0700

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,16 +7,15 @@
  * @h: pointer to head of list
  * Return: 1 if cyclic , 0 otherwise
  */
-int check_cycle(listint_t*list)
+int check_cycle(listint_t *list)
 {
     listint_t *slow = list, *fast = list;
+
     while (fast && fast->next)
     {
-        slow = slow->next;
-        fast = fast->next;
-	if (slow==fast)
-        	return (1);
+	        slow = slow->next;
+	        fast = fast->next;
+		if (slow==fast)
+        		return (1);
     }
-
     return (0);
-}
