9da94e0d2b00c8dd4d20b438ca70687564cf924d
Zainab990 <zainabomar998@gmail.com>
2024-05-09 12:50:13 -0700

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check cycle - checks if list is cyclic
+ * @h: pointer to head of list
+ * Return: 1 if cyclic , 0 otherwise
+ */
+int check_cycle(listint_t*list)
+{
+    listint_t *slow = list, *fast = list;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next;
+	if (slow==fast)
+        	return (1);
+    }
+
+    return (0);
+}
